4c347e1d44ef099c239b351906445ea1e3110be4
Patrick Perkins <plp248@nyu.edu>
2020-03-02 10:32:39 -0500

    commented the unclear and clarified variable name for formatted_csv_data

--- Quire.rb
+++ Quire.rb
@@ -41,12 +41,14 @@ class Quire
     @leaves << Leaf.new(parent_quire, position, folio_number, single)
   end
 
-  def add_leaves(leaf_hash)
-    parent_quire = leaf_hash['parent_quire'].to_i
-    position = leaf_hash['position']
-    fol_num = leaf_hash['fol_num']
-    single = leaf_hash['single']
-    add_leaf(parent_quire, position, fol_num, single)
+  def add_leaves(array_of_leaf_hashes)
+    array_of_leaf_hashes.each do |leaf_hash|
+      parent_quire = leaf_hash['parent_quire'].to_i
+      position = leaf_hash['position']
+      fol_num = leaf_hash['fol_num']
+      single = leaf_hash['single']
+      add_leaf(parent_quire, position, fol_num, single)
+    end
   end
 
   def size
@@ -79,26 +81,39 @@ CSV.foreach '/Users/patrick/work/collation_playground/quire_data.csv', headers: 
   row_hashes << row.to_h
 end
 
-# TODO auto detect how many quires in the input csv
-# right now we are manually creating quires and shoveling
-# data in. if we create quires based on how many variations
-# of 'parent_quire' there are in the spreadsheet. then,
-# we separate the csv data based on their 'parent_quire'
-# number, and shovel the data into the quires that we have created.
+# formatted_csv_data is the raw csv information in a nested structure
+# that imitates the construction of a book
 
-quire_array = []
+# quires is a similar structure, but makes use of the Quire and
+# Leaf object to arrange and modify our leaf information (ie conjoin)
+
+# formatted_csv_data is an array where each element is another array
+# of hashes. each hash contains the information from a row in
+# the csv
+
+formatted_csv_data = []
 row_hashes.each do |hash|
+  # convert the 'parent_quire' value from the csv to int and assign to
+  # variable called parent_quire '1'
   parent_quire = hash['parent_quire'].to_i
-  quire_array[parent_quire] = [] if quire_array[parent_quire].nil?
-  quire_array[parent_quire] << hash
+  # create empty array at index [parent_quire] 'quire_array[1]'
+  # if there isn't already a value there
+  formatted_csv_data[parent_quire] = [] if formatted_csv_data[parent_quire].nil?
+  # shovel in the leaf hash (csv row) information at the index of
+  # the parent quire
+  formatted_csv_data[parent_quire] << hash
 end
 
-# TODO this piece of code should create new quire objects and
-# shovel in the correct data to the correct quire
+# quires is an array where each element is a quire object. inside each quire
+# object is an array called @leaves. each element in this array is a page object.
 quires = []
-quire_array.each do |quire_leaves|
+formatted_csv_data.each do |quire_leaves|
   next if quire_leaves.nil?
-  quire_leaves.each { |leaf_hash| quires << leaf_hash }
+  quire = Quire.new
+  quire.add_leaves quire_leaves
+  quires << quire
 end
 
-puts quires
+quires.each do |quire|
+  quire.set_conjoins
+end
